3eb85f930e94c92a7c3189791927c8cfb65a5f41
heikeling <3542443243@qq.com>
2023-04-25 17:08:01 +0800

    First commit.

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ def index():
     ip = request.remote_addr
     data = request.form
     a = str(data.get("ky"))
-    s += ip+":"+a+"\n"
+    if a == 'None':
+        pass
+    else:
+        s += ip+":"+a+"\n"
     return render_template("index.html",texts = s)
 app.run(host='0.0.0.0',port="80")
